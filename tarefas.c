3065f836b0eca24c5eb4d02fb709aa7d2a285227
biancasio <161092766+biancasio@users.noreply.github.com>
2024-04-01 01:54:08 +0000

    feat: Função de criar Tarefas

--- tarefas.c
+++ tarefas.c
@@ -3,7 +3,24 @@
 #include "tarefas.h"
 
 ERROS criar(Tarefa tarefas[], int *pos){
-    printf("Função de Criar Tarefas\n");
+    //testa para ver se a quantidade de tarefas é maior que o total
+    if(*pos >= TOTAL)
+        return MAX_TAREFA;
+// se n for eu peço para entrar com a prioridade e a categoria
+    printf("Entre com a prioridade: ");
+    scanf("%d", &tarefas[*pos].prioridade);
+
+    clearBuffer();
+    printf("Entre com a categoria: ");
+    fgets(tarefas[*pos].categoria, 100, stdin);
+    
+
+    printf("Entre com a descrição: ");
+    fgets(tarefas[*pos].descricao, 300, stdin);
+
+ // soma 1+1 no valor da categoria
+    *pos = *pos + 1;
+
     return OK;
 }
 
@@ -25,4 +42,11 @@ ERROS salvar(Tarefa tarefas[], int *pos){
 ERROS carregar(Tarefa tarefas[], int *pos){
     printf("Função de Carregar Tarefas\n");
     return OK;
+}
+
+
+void clearBuffer(){
+    int c;
+    //lendo um caracter armazendo em c que n é um char mas é um inteiro, ao mesmo tempo compara para ver se é diferente do final do arquivo 
+    while ((c = getchar()) != '\n' && c != EOF){}
 }
